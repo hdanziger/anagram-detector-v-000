92f930f0e23bd03a0467f2d1c51523ef1daee486
Hannah Danziger <hdanziger8@gmail.com>
2017-09-28 16:10:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize
+    @words = words
+  end 
+
+  def match(words)
+    some_words = words.split("")
+    some_words.sort.match
+  end
+    
+    
+    
+  end
